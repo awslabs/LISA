b196aa37bcc968a92cf4e84a1e413e0981ba6b97
Kyle Lilly <ktlilly@amazon.com>
2024-05-15 08:20:27 -0400

    Add unit tests to the CDK application stacks capturing current posture of CDK baseline applied against controls. Move over pre-commit config.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,8 +13,8 @@
 	- Customers can now configure LISA to use PGVector in addition or in place of OpenSearch for their RAG repository. LISA can connect to an existing RDS Postgres instance or one can be deployed as part of the LISA deployment.
 	- PGVector is now the "default" configuration in the `config.yaml` file as PGVector is considerably faster to deploy for demo/test purposes
 - [V1282894257] Improved support for custom DNS
-	- Customers can now specify a custom domain name for the LISA API Gateway as well as the LISA Serve REST ALB. If these values are set in the `config.yaml` file the UI will automaticaly use the correct pathing when making service requests without needing any additional code changes.
-- [V1282858379] Move advanced chat configuration options between a collapsable "Advanced configuration" sectoin
+	- Customers can now specify a custom domain name for the LISA API Gateway as well as the LISA Serve REST ALB. If these values are set in the `config.yaml` file the UI will automatically use the correct pathing when making service requests without needing any additional code changes.
+- [V1282858379] Move advanced chat configuration options between a collapsible "Advanced configuration" sectoin
 	- The chat "control panel" has been redesigned to hide the following items chat buffer, model kwargs, prompt template, and the metadata toggle
 - [V1282855530] Add support for Enterprise CDK PermissionBoundaryAspect and custom synthesizer
 	- Added new property to the `config.yaml` to allow customers to optionally specify a configuration for the PermissionBoundaryAspect. If counfigured the aspect will be applied to all stacks
@@ -27,9 +27,9 @@
 	- Default is now an `m5.large` however customers can use any instance type they wish although they may need to update `schema.ts` if the instance type isn't already listed there
 
 #### Bugs
-- [AIML-ADC-7604] Rag context not visibile in metadata until subsequent messages
+- [AIML-ADC-7604] Rag context not visible in metadata until subsequent messages
 	- This has been addressed by adding a dedicated `ragContext` property to the message metadata. The relevant documents including s3 key will now be visible when sending a message with a RAG repository and embedding model selected.
-- [V1283663967] Chat messages ocassionaly wrap
+- [V1283663967] Chat messages occasionally wrap
 	- Sometimes the model returns text that gets wrapped in ``<pre><code> </code></pre>``. The default style for this content now has word-wrap and word-break styles applied to prevent the message overflowing.
 
 #### Additional changes
---
+++ test/cdk/stacks/networking.test.ts
@@ -0,0 +1,119 @@
+/**
+  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+  Licensed under the Apache License, Version 2.0 (the "License").
+  You may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+import * as fs from 'fs';
+import * as path from 'path';
+
+import { App, Aspects, Stack } from 'aws-cdk-lib';
+import { Annotations, Match } from 'aws-cdk-lib/assertions';
+import { AwsSolutionsChecks, NIST80053R5Checks } from 'cdk-nag';
+import * as yaml from 'js-yaml';
+
+import { createCdkId } from '../../../lib/core/utils';
+import { LisaNetworkingStack } from '../../../lib/networking/index';
+import { Config, ConfigSchema } from '../../../lib/schema';
+
+type TestConfig = [string, number, number, number, number];
+
+const regions: TestConfig[] = [
+  ['us-east-1', 1, 2, 3, 5 ],
+  ['us-gov-west-1', 1, 2, 3, 5 ],
+  ['us-gov-east-1', 1, 2, 3, 5 ],
+  ['us-isob-east-1', 1, 2, 3, 5 ],
+  ['us-iso-east-1', 1, 2, 3, 5 ],
+  ['us-iso-west-1', 1, 2, 3, 5 ],
+];
+
+
+describe.each<TestConfig>(regions)(
+  'Network Nag Pack Tests | Region Test: %s',
+  (awsRegion, awsSolWarns, awsSolErr, nistWarn, nistErr) => {
+    let app: App;
+    let stack: Stack;
+    let config: Config;
+    let baseStackProps: any;
+
+    beforeAll(() => {
+      app = new App();
+
+      // Read configuration file
+      const configFilePath = path.join(__dirname, '../mocks/config.yaml');
+      const configFile = yaml.load(fs.readFileSync(configFilePath, 'utf8')) as any;
+      const configEnv = configFile.env || 'dev';
+      const configData = configFile[configEnv];
+      if (!configData) {
+        throw new Error(`Configuration for environment "${configEnv}" not found.`);
+      }
+      // Validate and parse configuration
+      try {
+        config = ConfigSchema.parse(configData);
+      } catch (error) {
+        if (error instanceof Error) {
+          console.error('Error parsing the configuration:', error.message);
+        } else {
+          console.error('An unexpected error occurred:', error);
+        }
+        process.exit(1);
+      }
+
+      baseStackProps = {
+        env: {
+          account: '012345678901',
+          region: awsRegion,
+        },
+        config,
+      };
+    });
+
+    beforeEach(() => {
+      stack = new LisaNetworkingStack(app, `TestNetworking${awsRegion}`, {
+        ...baseStackProps,
+        config: config,
+        stackName: createCdkId([config.deploymentName, config.appName, 'networking', config.deploymentStage]),
+        description: `LISA-networking: ${config.deploymentName}-${config.deploymentStage}`,
+      });
+      // WHEN
+      Aspects.of(stack).add(new AwsSolutionsChecks({ verbose: true }));
+      Aspects.of(stack).add(new NIST80053R5Checks({ verbose: true }));
+    });
+
+    afterEach(() => {
+      app = new App();
+      stack = new Stack();
+    });
+
+    //TODO Update expect values to remediate CDK NAG findings and remove debug
+    test('AwsSolutions CDK NAG Warnings', () => {
+      const warnings = Annotations.fromStack(stack).findWarning('*', Match.stringLikeRegexp('AwsSolutions-.*'));
+      expect(warnings.length).toBe(awsSolWarns);
+    });
+
+    test('AwsSolutions CDK NAG Errors', () => {
+      const errors = Annotations.fromStack(stack).findError('*', Match.stringLikeRegexp('AwsSolutions-.*'));
+      expect(errors.length).toBe(awsSolErr);
+    });
+
+    test('NIST800.53r5 CDK NAG Warnings', () => {
+      const warnings = Annotations.fromStack(stack).findWarning('*', Match.stringLikeRegexp('NIST.*'));
+      expect(warnings.length).toBe(nistWarn);
+    });
+
+    test('NIST800.53r5 CDK NAG Errors', () => {
+      const errors = Annotations.fromStack(stack).findError('*', Match.stringLikeRegexp('NIST.*'));
+      expect(errors.length).toBe(nistErr);
+    });
+  },
+);

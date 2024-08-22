ee28ce849ef89f3b6a7d7c284c5d2638ca10974c
Jonathan Blackstock <jtblack@amazon.com>
2024-08-22 16:02:30 -0600

    feat: initial form skeleton and model

--- lib/user-interface/react/src/components/model-management/create-model/CreateModelModal.tsx
+++ lib/user-interface/react/src/components/model-management/create-model/CreateModelModal.tsx
@@ -15,12 +15,18 @@
  */
 import Form from '@cloudscape-design/components/form';
 import SpaceBetween from '@cloudscape-design/components/space-between';
-import { Button, Modal } from '@cloudscape-design/components';
-import Container from '@cloudscape-design/components/container';
+import { Button, ExpandableSection, Modal } from '@cloudscape-design/components';
 import FormField from '@cloudscape-design/components/form-field';
 import Input from '@cloudscape-design/components/input';
-import { IModel } from '../../../shared/model/model-management.model';
-import { ReactElement } from 'react';
+import {
+    IModel,
+    InferenceContainer,
+    ModelRequestSchema,
+    ModelType,
+} from '../../../shared/model/model-management.model';
+import { ReactElement, useState } from 'react';
+import Select from '@cloudscape-design/components/select';
+import Toggle from '@cloudscape-design/components/toggle';
 
 export type CreateModelModalProps = {
     visible: boolean;
@@ -30,6 +36,18 @@ export type CreateModelModalProps = {
 };
 
 export function CreateModelModal (props: CreateModelModalProps) : ReactElement {
+    console.log(props);
+    console.log(ModelRequestSchema.parse({}));
+
+    const [streaming, setStreaming] = useState(true);
+    const [
+        selectedModelType,
+        setSelectedModelType
+    ] = useState({ label: 'TEXTGEN', value: ModelType.textgen });
+    const [
+        selectedInferenceContainer,
+        setSelectedInferenceContainer
+    ] = useState({ label: 'TGI', value: InferenceContainer.TGI });
     return (
         <Modal onDismiss={() => props.setVisible(false)} visible={props.visible} header={`${props.isEdit ? 'Update' : 'Create'} Model`}>
             <form onSubmit={(e) => e.preventDefault()}>
@@ -43,19 +61,171 @@ export function CreateModelModal (props: CreateModelModalProps) : ReactElement {
                         </SpaceBetween>
                     }
                 >
-                    <Container>
-                        <SpaceBetween direction='vertical' size='l'>
-                            <FormField label='First field'>
-                                <Input value='' />
+                    <SpaceBetween direction='vertical' size='xs'>
+                        <FormField label='Model ID'>
+                            <Input value=''
+                                inputMode='text'/>
+                        </FormField>
+                        <FormField label='Model Name'>
+                            <Input value=''
+                                inputMode='text'/>
+                        </FormField>
+                        <FormField label='Streaming'>
+                            <Toggle
+                                onChange={({ detail }) =>
+                                    setStreaming(detail.checked)
+                                }
+                                checked={streaming}
+                            />
+                        </FormField>
+                        <FormField label='Model Type'>
+                            <Select
+                                selectedOption={selectedModelType}
+                                onChange={({ detail }) =>
+                                    setSelectedModelType(detail.selectedOption)
+                                }
+                                options={[
+                                    { label: 'TEXTGEN', value: ModelType.textgen },
+                                    { label: 'EMBEDDING', value: ModelType.embedding },
+                                ]}
+                            />
+                        </FormField>
+                        <FormField label='Instance Type'>
+                            <Input value=''
+                                inputMode='text'/>
+                        </FormField>
+                        <FormField label='Inference Container'>
+                            <Select
+                                selectedOption={selectedInferenceContainer}
+                                onChange={({ detail }) =>
+                                    setSelectedInferenceContainer(detail.selectedOption)
+                                }
+                                options={[
+                                    { label: 'TGI', value: InferenceContainer.TGI },
+                                    { label: 'TEI', value: InferenceContainer.TEI },
+                                    { label: 'VLLM', value: InferenceContainer.VLLM },
+                                    { label: 'INSTRUCTOR', value: InferenceContainer.INSTRUCTOR },
+                                ]}
+                            />
+                        </FormField>
+                        <ExpandableSection variant='stacked'
+                            headerText='Container Config'>
+                            <FormField label='Shared Memory Size'>
+                                <Input value=''
+                                    inputMode='numeric'/>
                             </FormField>
-                            <FormField label='Second field'>
-                                <Input value='' />
+                            <ExpandableSection defaultExpanded
+                                headingTagOverride='h3'
+                                variant='stacked'
+                                headerText='Base Image Config'>
+                                <FormField label='Base Image'>
+                                    <Input value=''
+                                        inputMode='text'/>
+                                </FormField>
+                                <FormField label='Path'>
+                                    <Input value=''
+                                        inputMode='text'/>
+                                </FormField>
+                                <FormField label='Type'>
+                                    <Input value=''
+                                        inputMode='text'/>
+                                </FormField>
+                            </ExpandableSection>
+                            <ExpandableSection defaultExpanded
+                                headingTagOverride='h3'
+                                variant='stacked'
+                                headerText='Container Health Check Config'>
+                                <FormField label='Command'>
+                                    <Input value=''
+                                        inputMode='text'/>
+                                </FormField>
+                                <FormField label='Interval'>
+                                    <Input value=''
+                                        inputMode='numeric'/>
+                                </FormField>
+                                <FormField label='Start Period'>
+                                    <Input value=''
+                                        inputMode='numeric'/>
+                                </FormField>
+                                <FormField label='Timeout'>
+                                    <Input value=''
+                                        inputMode='numeric'/>
+                                </FormField>
+                                <FormField label='Retries'>
+                                    <Input value=''
+                                        inputMode='numeric'/>
+                                </FormField>
+                            </ExpandableSection>
+                        </ExpandableSection>
+                        <ExpandableSection variant='stacked'
+                            headerText='Auto Scaling Config'>
+                            <FormField label='Min Capacity'>
+                                <Input value=''
+                                    inputMode='numeric'/>
                             </FormField>
-                            <FormField label='Third field'>
-                                <Input value='' />
+                            <FormField label='Max Capacity'>
+                                <Input value=''
+                                    inputMode='numeric'/>
                             </FormField>
-                        </SpaceBetween>
-                    </Container>
+                            <FormField label='Cooldown'>
+                                <Input value=''
+                                    inputMode='numeric'/>
+                            </FormField>
+                            <FormField label='Default Instance Warmup'>
+                                <Input value=''
+                                    inputMode='numeric'/>
+                            </FormField>
+                            <ExpandableSection defaultExpanded
+                                headingTagOverride='h3'
+                                variant='stacked'
+                                headerText='Metric Config'>
+                                <FormField label='ALB Metric Name'>
+                                    <Input value=''
+                                        inputMode='text'/>
+                                </FormField>
+                                <FormField label='Target Value'>
+                                    <Input value=''
+                                        inputMode='text'/>
+                                </FormField>
+                                <FormField label='Duration'>
+                                    <Input value=''
+                                        inputMode='numeric'/>
+                                </FormField>
+                                <FormField label='Estimated Instance Warmup'>
+                                    <Input value=''
+                                        inputMode='numeric'/>
+                                </FormField>
+                            </ExpandableSection>
+                        </ExpandableSection>
+                        <ExpandableSection variant='stacked'
+                            headerText='Load Balancer Config'>
+                            <ExpandableSection defaultExpanded
+                                headingTagOverride='h3'
+                                variant='stacked'
+                                headerText='Health Check Config'>
+                                <FormField label='Path'>
+                                    <Input value=''
+                                        inputMode='text'/>
+                                </FormField>
+                                <FormField label='Interval'>
+                                    <Input value=''
+                                        inputMode='numeric'/>
+                                </FormField>
+                                <FormField label='Timeout'>
+                                    <Input value=''
+                                        inputMode='numeric'/>
+                                </FormField>
+                                <FormField label='Healthy Threshold Count'>
+                                    <Input value=''
+                                        inputMode='numeric'/>
+                                </FormField>
+                                <FormField label='Unhealthy Threshold Count'>
+                                    <Input value=''
+                                        inputMode='numeric'/>
+                                </FormField>
+                            </ExpandableSection>
+                        </ExpandableSection>
+                    </SpaceBetween>
                 </Form>
             </form>
         </Modal>
--- lib/user-interface/react/src/shared/model/model-management.model.ts
+++ lib/user-interface/react/src/shared/model/model-management.model.ts
@@ -13,6 +13,7 @@
  See the License for the specific language governing permissions and
  limitations under the License.
  */
+import { z } from 'zod';
 
 export enum ModelStatus {
     Creating = 'Creating',
@@ -29,6 +30,61 @@ export enum ModelType {
     embedding = 'embedding',
 }
 
+export enum InferenceContainer {
+    TGI = 'tgi',
+    TEI = 'tei',
+    VLLM = 'vllm',
+    INSTRUCTOR = 'instructor',
+}
+
+export type IContainerHealthCheckConfig = {
+    Command: string[];
+    Interval: number;
+    StartPeriod: number;
+    Timeout: number;
+    Retries: number;
+};
+
+export type IContainerConfigImage = {
+    BaseImage: string;
+    Path: string;
+    Type: string;
+};
+
+export type IMetricConfig = {
+    AlbMetricName: string;
+    TargetValue: number;
+    Duration: number;
+    EstimatedInstanceWarmup: number;
+};
+
+export type ILoadBalancerHealthCheckConfig = {
+    Path: string;
+    Interval: number;
+    Timeout: number;
+    HealthyThresholdCount: number;
+    UnhealthyThresholdCount: number;
+};
+
+export type ILoadBalancerConfig = {
+    HealthCheckConfig: ILoadBalancerHealthCheckConfig
+};
+
+export type IAutoScalingConfig = {
+    MinCapacity: number;
+    MaxCapacity: number;
+    Cooldown: number;
+    DefaultInstanceWarmup: number;
+    MetricConfig: IMetricConfig;
+};
+
+export type IContainerConfig = {
+    BaseImage: IContainerConfigImage;
+    SharedMemorySize: number;
+    HealthCheckConfig: IContainerHealthCheckConfig;
+    Environment: Map<string, string>;
+};
+
 export type IModel = {
     ModelName: string;
     ModelId: string;
@@ -37,3 +93,76 @@ export type IModel = {
     Streaming: boolean;
     Status: ModelStatus;
 };
+
+export type IModelRequest = {
+    ModelId: string;
+    ModelName: string;
+    Streaming: boolean;
+    ModelType: ModelType;
+    InstanceType: string;
+    InferenceContainer: InferenceContainer;
+    ContainerConfig: IContainerConfig;
+    AutoScalingConfig: IAutoScalingConfig;
+    LoadBalancerConfig: ILoadBalancerConfig;
+};
+
+const containerHealthCheckConfigSchema = z.object({
+    Command: z.array(z.string()).default(['CMD-SHELL', 'exit 0']),
+    Interval: z.number().default(10),
+    StartPeriod: z.number().default(30),
+    Timeout: z.number().default(5),
+    Retries: z.number().default(2),
+});
+
+
+const containerConfigImageSchema = z.object({
+    BaseImage: z.string().default(''),
+    Path: z.string().default(''),
+    Type: z.string().default(''),
+});
+
+export const metricConfigSchema = z.object({
+    AlbMetricName: z.string().default(''),
+    TargetValue: z.number().default(0),
+    Duration: z.number().default(60),
+    EstimatedInstanceWarmup: z.number().default(180),
+});
+
+export const loadBalancerHealthCheckConfigSchema = z.object({
+    Path: z.string().default(''),
+    Interval: z.number().default(10),
+    Timeout: z.number().default(5),
+    HealthyThresholdCount: z.number().default(1),
+    UnhealthyThresholdCount: z.number().default(1),
+});
+
+export const loadBalancerConfigSchema = z.object({
+    HealthCheckConfig: loadBalancerHealthCheckConfigSchema.default(loadBalancerHealthCheckConfigSchema.parse({})),
+});
+
+export const autoScalingConfigSchema = z.object({
+    MinCapacity: z.number().min(1).default(1),
+    MaxCapacity: z.number().min(1).default(2),
+    Cooldown: z.number().min(1).default(420),
+    DefaultInstanceWarmup: z.number().default(180),
+    MetricConfig: metricConfigSchema.default(metricConfigSchema.parse({})),
+});
+
+export const containerConfigSchema = z.object({
+    BaseImage: containerConfigImageSchema.default(containerConfigImageSchema.parse({})),
+    SharedMemorySize: z.number().min(0).default(0),
+    HealthCheckConfig: containerHealthCheckConfigSchema.default(containerHealthCheckConfigSchema.parse({})),
+    Environment: z.map(z.string(), z.string()).default(new Map<string, string>()),
+});
+
+export const ModelRequestSchema = z.object({
+    ModelId: z.string().default(''),
+    ModelName: z.string().default(''),
+    Streaming: z.boolean().default(true),
+    ModelType: z.nativeEnum(ModelType).default(ModelType.textgen),
+    InstanceType: z.string().default(''),
+    InferenceContainer: z.nativeEnum(InferenceContainer).default(InferenceContainer.TEI),
+    ContainerConfig: containerConfigSchema.default(containerConfigSchema.parse({})),
+    AutoScalingConfig: autoScalingConfigSchema.default(autoScalingConfigSchema.parse({})),
+    LoadBalancerConfig: loadBalancerConfigSchema.default(loadBalancerConfigSchema.parse({})),
+});

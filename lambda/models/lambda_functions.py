ccd1394f1487d19e19f2c03476d3d691c4ffa137
Peter Muller <petermul@amazon.com>
2024-08-28 08:39:24 -0700

    Update API model shapes

--- lambda/models/lambda_functions.py
+++ lambda/models/lambda_functions.py
@@ -13,8 +13,10 @@
 #   limitations under the License.
 
 """APIGW endpoints for managing models."""
-
 from typing import Annotated
+
+# Remove the following imports after APIs are no longer stubbed
+from uuid import uuid4
 
 from fastapi import FastAPI, Path
 from fastapi.middleware.cors import CORSMiddleware
@@ -22,15 +24,25 @@ from mangum import Mangum
 from utilities.fastapi_middleware.aws_api_gateway_middleware import AWSAPIGatewayMiddleware
 
 from .domain_objects import (
+    AutoScalingConfig,
     ContainerConfig,
+    ContainerConfigImage,
+    ContainerHealthCheckConfig,
     CreateModelRequest,
     CreateModelResponse,
     DeleteModelResponse,
-    DescribeModelResponse,
-    ListModelResponse,
+    GetModelResponse,
+    LISAModel,
+    ListModelsResponse,
+    LoadBalancerConfig,
+    LoadBalancerHealthCheckConfig,
+    MetricConfig,
     ModelStatus,
     ModelType,
+    StartModelResponse,
+    StopModelResponse,
     UpdateModelRequest,
+    UpdateModelResponse,
 )
 
 app = FastAPI(redirect_slashes=False, lifespan="off")
@@ -48,111 +60,147 @@ app.add_middleware(
 
 @app.post(path="", include_in_schema=False)  # type: ignore
 @app.post(path="/")  # type: ignore
-async def create_model(model: CreateModelRequest) -> CreateModelResponse:
+async def create_model(create_request: CreateModelRequest) -> CreateModelResponse:
     """Endpoint to create a model."""
     # TODO add service to create model
-    return CreateModelResponse(ModelId=model.ModelId, ModelName=model.ModelName, Status=ModelStatus.CREATING)
+    return CreateModelResponse(
+        Model=_create_dummy_model(create_request.ModelName, ModelType.TEXTGEN, ModelStatus.CREATING)
+    )
 
 
 @app.get(path="", include_in_schema=False)  # type: ignore
 @app.get(path="/")  # type: ignore
-async def list_models() -> list[ListModelResponse]:
+async def list_models() -> ListModelsResponse:
     """Endpoint to list models."""
-    # TODO add service to list models
-    return [
-        _create_dummy_model("my_first_model", ModelType.TEXTGEN, ModelStatus.CREATING),
-        _create_dummy_model("my_second_model", ModelType.EMBEDDING, ModelStatus.IN_SERVICE),
-        _create_dummy_model("my_third_model", ModelType.EMBEDDING, ModelStatus.STOPPING),
-        _create_dummy_model("my_fourth_model", ModelType.TEXTGEN, ModelStatus.STOPPED),
-        _create_dummy_model("my_fifth_model", ModelType.EMBEDDING, ModelStatus.UPDATING),
-        _create_dummy_model("my_sixth_model", ModelType.EMBEDDING, ModelStatus.DELETING),
-        _create_dummy_model("my_seventh_model", ModelType.TEXTGEN, ModelStatus.FAILED),
-        _create_dummy_model("my_eighth_model", ModelType.TEXTGEN, ModelStatus.FAILED),
-        _create_dummy_model("my_ninth_model", ModelType.EMBEDDING, ModelStatus.DELETING),
-        _create_dummy_model("my_tenth_model", ModelType.TEXTGEN, ModelStatus.UPDATING),
-        _create_dummy_model("my_eleventh_model", ModelType.EMBEDDING, ModelStatus.STOPPED),
-        _create_dummy_model("my_twelfth_model", ModelType.EMBEDDING, ModelStatus.STOPPING),
-        _create_dummy_model("my_thirteenth_model", ModelType.TEXTGEN, ModelStatus.IN_SERVICE),
-        _create_dummy_model("my_fourteenth_model", ModelType.TEXTGEN, ModelStatus.CREATING),
-        _create_dummy_model("my_fifteenth_model", ModelType.EMBEDDING, ModelStatus.CREATING),
-        _create_dummy_model("my_sixteenth_model", ModelType.TEXTGEN, ModelStatus.IN_SERVICE),
-        _create_dummy_model("my_seventeenth_model", ModelType.EMBEDDING, ModelStatus.STOPPING),
-        _create_dummy_model("my_eighteenth_model", ModelType.EMBEDDING, ModelStatus.STOPPED),
-        _create_dummy_model("my_nineteenth_model", ModelType.EMBEDDING, ModelStatus.UPDATING),
-        _create_dummy_model("my_twentieth_model", ModelType.TEXTGEN, ModelStatus.DELETING),
-        _create_dummy_model("my_twenty_first_model", ModelType.TEXTGEN, ModelStatus.FAILED),
-        _create_dummy_model("my_twenty_second_model", ModelType.EMBEDDING, ModelStatus.FAILED),
-        _create_dummy_model("my_twenty_third_model", ModelType.EMBEDDING, ModelStatus.DELETING),
-        _create_dummy_model("my_twenty_fourth_model", ModelType.TEXTGEN, ModelStatus.UPDATING),
-        _create_dummy_model("my_twenty_fifth_model", ModelType.TEXTGEN, ModelStatus.STOPPED),
-        _create_dummy_model("my_twenty_sixth_model", ModelType.TEXTGEN, ModelStatus.IN_SERVICE),
-    ]
+    return ListModelsResponse(
+        Models=[
+            _create_dummy_model("my_first_model", ModelType.TEXTGEN, ModelStatus.CREATING),
+            _create_dummy_model("my_second_model", ModelType.EMBEDDING, ModelStatus.IN_SERVICE),
+            _create_dummy_model("my_third_model", ModelType.EMBEDDING, ModelStatus.STOPPING),
+            _create_dummy_model("my_fourth_model", ModelType.TEXTGEN, ModelStatus.STOPPED),
+            _create_dummy_model("my_fifth_model", ModelType.EMBEDDING, ModelStatus.UPDATING),
+            _create_dummy_model("my_sixth_model", ModelType.EMBEDDING, ModelStatus.DELETING),
+            _create_dummy_model("my_seventh_model", ModelType.TEXTGEN, ModelStatus.FAILED),
+            _create_dummy_model("my_eighth_model", ModelType.TEXTGEN, ModelStatus.FAILED),
+            _create_dummy_model("my_ninth_model", ModelType.EMBEDDING, ModelStatus.DELETING),
+            _create_dummy_model("my_tenth_model", ModelType.TEXTGEN, ModelStatus.UPDATING),
+            _create_dummy_model("my_eleventh_model", ModelType.EMBEDDING, ModelStatus.STOPPED),
+            _create_dummy_model("my_twelfth_model", ModelType.EMBEDDING, ModelStatus.STOPPING),
+            _create_dummy_model("my_thirteenth_model", ModelType.TEXTGEN, ModelStatus.IN_SERVICE),
+            _create_dummy_model("my_fourteenth_model", ModelType.TEXTGEN, ModelStatus.CREATING),
+            _create_dummy_model("my_fifteenth_model", ModelType.EMBEDDING, ModelStatus.CREATING),
+            _create_dummy_model("my_sixteenth_model", ModelType.TEXTGEN, ModelStatus.IN_SERVICE),
+            _create_dummy_model("my_seventeenth_model", ModelType.EMBEDDING, ModelStatus.STOPPING),
+            _create_dummy_model("my_eighteenth_model", ModelType.EMBEDDING, ModelStatus.STOPPED),
+            _create_dummy_model("my_nineteenth_model", ModelType.EMBEDDING, ModelStatus.UPDATING),
+            _create_dummy_model("my_twentieth_model", ModelType.TEXTGEN, ModelStatus.DELETING),
+            _create_dummy_model("my_twenty_first_model", ModelType.TEXTGEN, ModelStatus.FAILED),
+            _create_dummy_model("my_twenty_second_model", ModelType.EMBEDDING, ModelStatus.FAILED),
+            _create_dummy_model("my_twenty_third_model", ModelType.EMBEDDING, ModelStatus.DELETING),
+            _create_dummy_model("my_twenty_fourth_model", ModelType.TEXTGEN, ModelStatus.UPDATING),
+            _create_dummy_model("my_twenty_fifth_model", ModelType.TEXTGEN, ModelStatus.STOPPED),
+            _create_dummy_model("my_twenty_sixth_model", ModelType.TEXTGEN, ModelStatus.IN_SERVICE),
+        ]
+    )
 
 
-def _create_dummy_model(model_name: str, model_type: ModelType, model_status: ModelStatus) -> ListModelResponse:
-    return ListModelResponse(
+def _create_dummy_model(model_name: str, model_type: ModelType, model_status: ModelStatus) -> LISAModel:
+    return LISAModel(
+        UniqueId=str(uuid4()),
         ModelId=f"{model_name}_id",
         ModelName=model_name,
         ModelType=model_type,
         Status=model_status,
         Streaming=model_type == ModelType.TEXTGEN,
         ModelUrl="http://some.cool.alb.amazonaws.com/path/to/endpoint",
-        ContainerConfig=ContainerConfig.DUMMY(),
+        ContainerConfig=ContainerConfig(
+            BaseImage=ContainerConfigImage(
+                BaseImage="ghcr.io/huggingface/text-generation-inference:2.0.1",
+                Path="lib/serve/ecs-model/textgen/tgi",
+                Type="asset",
+            ),
+            SharedMemorySize=2048,
+            HealthCheckConfig=ContainerHealthCheckConfig(
+                Command=["CMD-SHELL", "exit 0"], Interval=10, StartPeriod=30, Timeout=5, Retries=5
+            ),
+            Environment={
+                "MAX_CONCURRENT_REQUESTS": "128",
+                "MAX_INPUT_LENGTH": "1024",
+                "MAX_TOTAL_TOKENS": "2048",
+            },
+        ),
+        AutoScalingConfig=AutoScalingConfig(
+            MinCapacity=1,
+            MaxCapacity=1,
+            Cooldown=60,
+            DefaultInstanceWarmup=60,
+            MetricConfig=MetricConfig(
+                AlbMetricName="RequestCountPerTarget",
+                TargetValue=1000,
+                Duration=60,
+                EstimatedInstanceWarmup=30,
+            ),
+        ),
+        LoadBalancerConfig=LoadBalancerConfig(
+            HealthCheckConfig=LoadBalancerHealthCheckConfig(
+                Path="/health",
+                Interval=60,
+                Timeout=30,
+                HealthyThresholdCount=2,
+                UnhealthyThresholdCount=10,
+            ),
+        ),
     )
 
 
-@app.get(path="/{model_id}")  # type: ignore
+@app.get(path="/{unique_id}")  # type: ignore
 async def get_model(
-    model_id: Annotated[str, Path(title="The name of the model to get")],
-) -> DescribeModelResponse:
+    unique_id: Annotated[str, Path(title="The unique model ID of the model to get")],
+) -> GetModelResponse:
     """Endpoint to describe a model."""
     # TODO add service to get model
-    return DescribeModelResponse.DUMMY(model_id, model_id)
+    model = _create_dummy_model("model_name", ModelType.TEXTGEN, ModelStatus.IN_SERVICE)
+    model.UniqueId = unique_id
+    return GetModelResponse(Model=model)
 
 
-@app.put(path="/{model_id}")  # type: ignore
-async def put_model(
-    model_id: Annotated[str, Path(title="The name of the model to update")], model: UpdateModelRequest
-) -> DescribeModelResponse:
+@app.put(path="/{unique_id}")  # type: ignore
+async def update_model(
+    unique_id: Annotated[str, Path(title="The unique model ID of the model to update")],
+    update_request: UpdateModelRequest,
+) -> UpdateModelResponse:
     """Endpoint to update a model."""
     # TODO add service to update model
-    model.Status = ModelStatus.UPDATING
-    return DescribeModelResponse(**model.model_dump())
+    model = _create_dummy_model("model_name", ModelType.TEXTGEN, ModelStatus.UPDATING)
+    model.UniqueId = unique_id
+    return UpdateModelResponse(model=unique_id)
 
 
-@app.put(path="/{model_id}/start")  # type: ignore
-async def start_model(model_id: Annotated[str, Path(title="The name of the model to start")]) -> ListModelResponse:
+@app.put(path="/{unique_id}/start")  # type: ignore
+async def start_model(
+    unique_id: Annotated[str, Path(title="The unique model ID of the model to start")]
+) -> StartModelResponse:
     """Endpoint to start a model."""
     # TODO add service to update model
-    return ListModelResponse(
-        ModelId=model_id,
-        ModelName=model_id,
-        ModelType=ModelType.TEXTGEN,
-        Status=ModelStatus.CREATING,
-        Streaming=True,
-        ModelUrl="http://some.cool.alb.amazonaws.com/path/to/endpoint",
-        ContainerConfig=ContainerConfig.DUMMY(),
-    )
+    model = _create_dummy_model("model_name", ModelType.TEXTGEN, ModelStatus.CREATING)
+    model.UniqueId = unique_id
+    return StartModelResponse(Model=model)
 
 
-@app.put(path="/{model_id}/stop")  # type: ignore
-async def stop_model(model_id: Annotated[str, Path(title="The name of the model to stop")]) -> ListModelResponse:
+@app.put(path="/{unique_id}/stop")  # type: ignore
+async def stop_model(
+    unique_id: Annotated[str, Path(title="The unique model ID of the model to stop")]
+) -> StopModelResponse:
     """Endpoint to stop a model."""
     # TODO add service to update model
-    return ListModelResponse(
-        ModelId=model_id,
-        ModelName=model_id,
-        ModelType=ModelType.TEXTGEN,
-        Status=ModelStatus.STOPPING,
-        Streaming=True,
-        ModelUrl="http://some.cool.alb.amazonaws.com/path/to/endpoint",
-        ContainerConfig=ContainerConfig.DUMMY(),
-    )
+    model = _create_dummy_model("model_name", ModelType.TEXTGEN, ModelStatus.STOPPING)
+    model.UniqueId = unique_id
+    return StopModelResponse(Model=model)
 
 
 @app.delete(path="/{model_id}")  # type: ignore
 async def delete_model(
-    model_id: Annotated[str, Path(title="The name of the model to delete")],
+    model_id: Annotated[str, Path(title="The unique model ID of the model to delete")],
 ) -> DeleteModelResponse:
     """Endpoint to delete a model."""
     # TODO add service to delete model

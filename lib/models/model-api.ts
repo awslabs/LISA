6b25937addfa13d03872f1faa1cd7930155baf52
Peter Muller <petermul@amazon.com>
2024-08-29 11:16:05 -0700

    Add missing GetServerCertificate permission

--- lib/models/model-api.ts
+++ lib/models/model-api.ts
@@ -18,13 +18,14 @@ import crypto from 'node:crypto';
 
 import { IAuthorizer, RestApi } from 'aws-cdk-lib/aws-apigateway';
 import { ISecurityGroup, IVpc } from 'aws-cdk-lib/aws-ec2';
-import { IRole } from 'aws-cdk-lib/aws-iam';
+import { Effect, IRole, Policy, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { LayerVersion } from 'aws-cdk-lib/aws-lambda';
 import { StringParameter } from 'aws-cdk-lib/aws-ssm';
 import { Construct } from 'constructs';
 
 import { PythonLambdaFunction, registerAPIEndpoint } from '../api-base/utils';
 import { BaseProps } from '../schema';
+import { add } from 'husky';
 
 /**
  * Properties for ModelsApi Construct.
@@ -107,6 +108,19 @@ export class ModelsApi extends Construct {
             securityGroups,
         );
         lisaServeEndpointUrlPs.grantRead(lambdaFunction.role!);
+
+        if (config.restApiConfig.loadBalancerConfig.sslCertIamArn) {
+            const additionalPerms = new Policy(this, 'ModelsApiAdditionalPerms', {
+                statements: [
+                    new PolicyStatement({
+                        actions: ['iam:GetServerCertificate'],
+                        resources: [config.restApiConfig.loadBalancerConfig.sslCertIamArn],
+                        effect: Effect.ALLOW,
+                    })
+                ]
+            });
+            lambdaFunction.role!.attachInlinePolicy(additionalPerms);
+        }
 
         const apis: PythonLambdaFunction[] = [
             // create endpoint for /models without a trailing slash but reuse

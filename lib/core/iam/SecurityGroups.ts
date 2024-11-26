a9e64530c6b4010f490e013d46db74cee3fa34df
bedanley <bedanley@amazon.com>
2024-11-26 14:50:04 -0700

    sg override fixes

--- lib/core/iam/SecurityGroups.ts
+++ lib/core/iam/SecurityGroups.ts
@@ -15,13 +15,25 @@
  */
 
 /**
- * List of all security groups used for overrides
+ * List of all security group ids used for overrides
  */
-export enum SecurityGroups {
-    LITE_LLM_SG = 'LiteLLMScalingSg',
+export enum SecurityGroupEnum {
+    LITE_LLM_SG = 'LISA-LiteLLMScalingSg',
     ECS_MODEL_ALB_SG = 'EcsModelAlbSg',
     REST_API_ALB_SG = 'RestApiAlbSg',
-    LAMBDA_SG = 'LambdaSg',
-    OPEN_SEARCH_SG = 'OpenSearchSg',
-    PG_VECTOR_SG = 'PGVectorSg',
+    LAMBDA_SG = 'LambdaSecurityGroup',
+    OPEN_SEARCH_SG = 'LISA-OpenSearchSg',
+    PG_VECTOR_SG = 'LISA-PGVectorSg',
 }
+
+/**
+ * List of all security group names used for overrides.
+ * LiteLLMScalingSg does not have a predefined name
+ */
+export const SecurityGroupNames: Record<string, string> = {
+    'EcsModelAlbSg' : 'ECS-ALB-SG',
+    'RestApiAlbSg' : 'RestAPI-ALB-SG',
+    'LambdaSecurityGroup' : 'Lambda-SG',
+    'LISA-OpenSearchSg' : 'OpenSearch-SG',
+    'LISA-PGVectorSg' : 'LISA-PGVector-SG',
+};

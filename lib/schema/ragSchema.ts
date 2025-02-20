8592e60768ebbaa2d1fc6c23a1fcd6958786dd6e
Bear Danley <bedanley@amazon.com>
2025-02-20 15:23:05 -0700

    rebase

--- lib/schema/ragSchema.ts
+++ lib/schema/ragSchema.ts
@@ -1,18 +1,18 @@
 /**
-  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
-  Licensed under the Apache License, Version 2.0 (the "License").
-  You may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
+ Licensed under the Apache License, Version 2.0 (the "License").
+ You may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-      http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-*/
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 import { z } from 'zod';
 import { EbsDeviceVolumeType } from './cdk';
 
@@ -63,8 +63,6 @@ export type OpenSearchConfig =
     z.infer<typeof OpenSearchNewClusterConfig>
     | z.infer<typeof OpenSearchExistingClusterConfig>;
 
-
-
 export const RdsInstanceConfig = z.object({
     username: z.string().default('postgres').describe('The username used for database connection.'),
     passwordSecretId: z.string().optional().describe('The SecretsManager Secret ID that stores the existing database password.'),
@@ -72,13 +70,17 @@ export const RdsInstanceConfig = z.object({
     dbName: z.string().default('postgres').describe('The name of the database for the database instance.'),
     dbPort: z.number().default(5432).describe('The port of the existing database instance or the port to be opened on the database instance.'),
 }).describe('Configuration schema for RDS Instances needed for LiteLLM scaling or PGVector RAG operations.\n \n ' +
-        'The optional fields can be omitted to create a new database instance, otherwise fill in all fields to use an existing database instance.');
+    'The optional fields can be omitted to create a new database instance, otherwise fill in all fields to use an existing database instance.');
 
 export type RdsConfig = z.infer<typeof RdsInstanceConfig>;
 
 export const RagRepositoryConfigSchema = z
     .object({
-        repositoryId: z.string().nonempty().describe('A unique identifier for the repository, used in API calls and the UI. It must be distinct across all repositories.'),
+        repositoryId: z.string()
+            .nonempty()
+            .regex(/^[a-z0-9-]{1,63}/, 'Only alphanumeric characters and \'-\' are supported.')
+            .regex(/^(?!-).*(?<!-)$/, 'Cannot start or end with a \'-\'.')
+            .describe('A unique identifier for the repository, used in API calls and the UI. It must be distinct across all repositories.'),
         repositoryName: z.string().optional().describe('The user-friendly name displayed in the UI.'),
         type: z.nativeEnum(RagRepositoryType).describe('The vector store designated for this repository.'),
         opensearchConfig: z.union([OpenSearchExistingClusterConfig, OpenSearchNewClusterConfig]).optional(),

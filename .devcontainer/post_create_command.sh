cc8d7f917ab3352fcc5af45d3bd5bed8ec0f4c9f
Dustin Sweigart <dustinps@amazon.com>
2025-03-28 15:59:45 +0000

    add dev container for standardized development environment

---
+++ .devcontainer/post_create_command.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+pwd
+
+sudo apt-get update -y
+sudo apt-get install -y jq yq
+
+make createPythonEnvironment
+. .venv/bin/activate
+echo "source .venv/bin/activate" >> ~/.bashrc
+echo "source .venv/bin/activate" >> ~/.zshrc
+
+pip install --upgrade pip
+pip3 install yq huggingface_hub s5cmd
+make installPythonRequirements
+
+make createTypeScriptEnvironment
+make installTypeScriptRequirements
+
+git config --unset-all core.hooksPath
+pre-commit install

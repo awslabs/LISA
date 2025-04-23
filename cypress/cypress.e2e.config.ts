19b6408ffaed9bd2e33e38883729c6c62c80c8f3
Andrew Batzel <batzela@amazon.com>
2025-04-23 09:00:09 -0600

    Updated Cypress config files to include correct pathing to project files.

--- cypress/cypress.e2e.config.ts
+++ cypress/cypress.e2e.config.ts
@@ -1,19 +1,22 @@
 /// <reference types="node" />
 
 import { defineConfig } from 'cypress';
+import path from 'path';
+
+const PROJECT_ROOT = path.resolve(__dirname);
 
 export default defineConfig({
     video: true,                        // turn on video recording
     videoCompression: true,
-    videosFolder: 'videos/e2e',     // where to save .mp4 files
+    videosFolder: `${PROJECT_ROOT}/videos/e2e`,     // where to save .mp4 files
     screenshotOnRunFailure: true,       // auto‑snap on any test failure
-    screenshotsFolder: 'screenshots/e2e',
+    screenshotsFolder: `${PROJECT_ROOT}/screenshots/e2e`,
     trashAssetsBeforeRuns: true,        // wipe out old videos/screenshots
     e2e: {
-        specPattern: 'src/e2e/specs/**/*.e2e.spec.ts',
-        supportFile: 'src/e2e/support/index.ts',
+        specPattern: `${PROJECT_ROOT}/src/e2e/specs/**/*.e2e.spec.ts`,
+        supportFile: `${PROJECT_ROOT}/src/e2e/support/index.ts`,
         experimentalStudio: true,
-        fixturesFolder: 'src/e2e/fixtures',
+        fixturesFolder: `${PROJECT_ROOT}/src/e2e/fixtures`,
         setupNodeEvents () {
         },
         baseUrl: 'https://5bma74uv9c.execute-api.us-east-1.amazonaws.com/dev',
--- cypress/cypress.smoke.config.ts
+++ cypress/cypress.smoke.config.ts
@@ -1,17 +1,21 @@
 import { defineConfig } from 'cypress';
+import path from 'path';
+
+const PROJECT_ROOT = path.resolve(__dirname);
 
 export default defineConfig({
     video: true,                        // turn on video recording
-    videosFolder: 'videos/smoke',     // where to save .mp4 files
+    videoCompression: true,
+    videosFolder: `${PROJECT_ROOT}/videos/smoke`,     // where to save .mp4 files
     screenshotOnRunFailure: true,       // auto‑snap on any test failure
-    screenshotsFolder: 'screenshots/smoke',
+    screenshotsFolder: `${PROJECT_ROOT}/screenshots/smoke`,
     trashAssetsBeforeRuns: true,        // wipe out old videos/screenshots
 
     e2e: {
-        specPattern: 'src/smoke/specs/**/*.smoke.spec.ts',
-        supportFile: 'src/smoke/support/index.ts',
+        specPattern: `${PROJECT_ROOT}/src/smoke/specs/**/*.smoke.spec.ts`,
+        supportFile: `${PROJECT_ROOT}/src/smoke/support/index.ts`,
         experimentalStudio: true,
-        fixturesFolder: 'src/smoke/fixtures',
+        fixturesFolder: `${PROJECT_ROOT}/src/smoke/fixtures`,
         setupNodeEvents () {
         },
         baseUrl: 'https://5bma74uv9c.execute-api.us-east-1.amazonaws.com/dev',

e54cd192d5509d928df03daa047015f64812073f
Evan Stohlmann <evmann@amazon.com>
2025-05-16 09:46:44 -0600

    Updating to properly show logo with custom domain

--- lib/user-interface/react/src/components/Topbar.tsx
+++ lib/user-interface/react/src/components/Topbar.tsx
@@ -87,7 +87,7 @@ function Topbar ({ configs }: TopbarProps): ReactElement {
             identity={{
                 href: useHref('/'),
                 logo: {
-                    src: `${getBaseURI()}/logo.svg`,
+                    src: `${getBaseURI() !== '/' ? getBaseURI() : ''}/logo.svg`,
                     alt: 'AWS LISA Sample',
                 },
             }}

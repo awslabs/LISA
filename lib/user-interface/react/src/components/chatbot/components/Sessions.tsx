e7396c084adcfa55ca0501ccbee86932cb75bd24
Andrew Batzel <batzela@amazon.com>
2025-06-04 17:50:42 +0000

    Bugfix for continuous calling of configuration endpoint

--- lib/user-interface/react/src/components/chatbot/components/Sessions.tsx
+++ lib/user-interface/react/src/components/chatbot/components/Sessions.tsx
@@ -142,7 +142,7 @@ export function Sessions ({ newSession }) {
                 }
             });
         }
-    }, [auth, getConfiguration]);
+    }, [auth.isLoading, auth.isAuthenticated, getConfiguration]);
 
     useEffect(() => {
         if (!isDeleteByIdLoading && isDeleteByIdSuccess) {

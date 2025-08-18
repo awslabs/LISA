9559263f1159b8a92a6a6d4f471d8887fea339d2
Evan Stohlmann <evmann@amazon.com>
2025-08-18 09:43:30 -0400

    test Bedrock KB queries

--- lambda/utilities/bedrock_kb.py
+++ lambda/utilities/bedrock_kb.py
@@ -1,3 +1,17 @@
+#   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License").
+#   You may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 """Utilities for handling Bedrock Knowledge Base specific operations.
 
 This module centralizes logic related to repositories of type
@@ -8,7 +22,6 @@ from __future__ import annotations
 
 import os
 from typing import Any, Dict, List
-
 
 BEDROCK_KB_TYPE = "bedrock_knowledge_base"
 
@@ -47,11 +60,7 @@ def retrieve_documents(
 
     docs: List[Dict[str, Any]] = []
     for doc in response.get("retrievalResults", []):
-        uri = (
-            (doc.get("location", {}) or {})
-            .get("s3Location", {})
-            .get("uri")
-        )
+        uri = (doc.get("location", {}) or {}).get("s3Location", {}).get("uri")
         name = uri.split("/")[-1] if uri else None
         docs.append(
             {
@@ -105,5 +114,3 @@ def delete_document_from_kb(
         knowledgeBaseId=bedrock_config.get("bedrockKnowledgeBaseId", None),
         dataSourceId=bedrock_config.get("bedrockKnowledgeDatasourceId", None),
     )
-
-

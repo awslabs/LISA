02329ce13f12ee0835eb613fe75cc434a01ef9e4
jmharold <jmharold@amazon.com>
2025-09-04 21:39:06 +0000

    pre-commit

--- lib/user-interface/react/src/components/chatbot/components/Message.tsx
+++ lib/user-interface/react/src/components/chatbot/components/Message.tsx
@@ -354,7 +354,7 @@ export default function Message ({ message, isRunning, showMetadata, isStreaming
                                     ...message.metadata,
                                     ...(message.usage && { usage: message.usage })
                                 }} style={darkStyles} />
-                        </ExpandableSection>}
+                            </ExpandableSection>}
                     </ChatBubble>
                     {!isStreaming && !messageContainsImage(message.content) && <div
                         style={{ display: 'flex', alignItems: 'center', height: '100%', justifyContent: 'flex-end' }}>

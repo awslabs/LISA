b5603ad5925bbff154ba78cdad7e5caf0c689d15
Andrew Batzel <batzela@amazon.com>
2025-10-14 14:49:41 -0600

    gpt oss compatability

--- lib/schema/configSchema.ts
+++ lib/schema/configSchema.ts
@@ -461,7 +461,8 @@ export const ContainerConfigSchema = z.object({
         .describe('Environment variables for the container.'),
     sharedMemorySize: z.number().min(0).default(0).describe('The value for the size of the /dev/shm volume.'),
     healthCheckConfig: ContainerHealthCheckConfigSchema.default({}),
-    privileged: z.boolean().optional()
+    privileged: z.boolean().optional(),
+    memoryReservation: z.number().min(0).optional().describe('Memory reservation in MiB for the container.')
 }).describe('Configuration for the container.');
 
 export type ContainerConfig = z.infer<typeof ContainerConfigSchema>;

d56da2f4bcf88f3bbb6dc4da84873df3c23d02f1
Bear Danley <bedanley@amazon.com>
2025-12-17 22:35:45 +0000

    Update tailwind config

--- lib/user-interface/react/vite.config.ts
+++ lib/user-interface/react/vite.config.ts
@@ -17,10 +17,11 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import { resolve } from 'node:path';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), tailwindcss()] as any,
     server: {
         port: 3000,
     },

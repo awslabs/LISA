8e16a1d37e7cfaa69d1faac79fc95a158efd1eec
Bear Danley <bedanley@amazon.com>
2026-01-14 21:39:12 -0700

    Remove setState within useEffect

--- lib/user-interface/react/src/components/repository-management/createRepository/CreateRepositoryModal.tsx
+++ lib/user-interface/react/src/components/repository-management/createRepository/CreateRepositoryModal.tsx
@@ -269,22 +269,22 @@ export function CreateRepositoryModal (props: CreateRepositoryModalProps): React
     }
 
     return (
-        <Modal 
+        <Modal
             data-testid='create-repository-modal'
-            size={'large'} 
+            size={'large'}
             onDismiss={() => {
-            dispatch(
-                setConfirmationModal({
-                    action: 'Discard',
-                    resourceName: 'Model Creation',
-                    onConfirm: () => {
-                        setVisible(false);
-                        setIsEdit(false);
-                        resetState();
-                    },
-                    description: 'Are you sure you want to discard your changes?',
-                }));
-        }} visible={visible} header={`${isEdit ? 'Update' : 'Create'} Repository`}>
+                dispatch(
+                    setConfirmationModal({
+                        action: 'Discard',
+                        resourceName: 'Model Creation',
+                        onConfirm: () => {
+                            setVisible(false);
+                            setIsEdit(false);
+                            resetState();
+                        },
+                        description: 'Are you sure you want to discard your changes?',
+                    }));
+            }} visible={visible} header={`${isEdit ? 'Update' : 'Create'} Repository`}>
             <Wizard
                 data-testid='create-repository-wizard'
                 i18nStrings={{
